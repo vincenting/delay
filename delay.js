f1e96803b762972fe22e36076dbe3cc16bfad21a
Vincent Ting <homerdd@gmail.com>
2014-12-07 15:48:50 +0800

    protect private function

--- delay.js
+++ delay.js
@@ -5,7 +5,7 @@
 !function (window) {
 
     var Delay = window.Delay = function () {
-        this._init.apply(this, arguments);
+        _init.apply(this, arguments);
     };
 
     var clearObject = function (o) {
@@ -18,7 +18,7 @@
 
     var fn = Delay.prototype;
 
-    fn._init = function () {
+    var _init = function () {
         // sign for whether queue has run.
         this._started = false;
         this._delayMap = {};
@@ -29,18 +29,18 @@
     };
 
     // generate unique id
-    fn._uniqueId = function () {
+    var _uniqueId = function () {
         var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
         return randLetter + Date.now();
     };
 
-    fn._startedNeeded = function (funcName) {
+    var _startedNeeded = function (funcName) {
         if (!this._started) {
             throw Error('can not call ' + funcName + ' before delay started.')
         }
     };
 
-    fn._notStartedNeeded = function (funcName) {
+    var _notStartedNeeded = function (funcName) {
         if (this._started) {
             throw Error('can not call ' + funcName + ' after delay started.')
         }
@@ -48,10 +48,10 @@
 
     // add task for delay queue.
     fn.add = function (duration, task, context) {
-        this._notStartedNeeded('add');
+        _notStartedNeeded.call(this, 'add');
         var taskId;
         while (true) {
-            taskId = this._uniqueId();
+            taskId = _uniqueId.call(this);
             if (!this._taskIdIndex.hasOwnProperty(taskId)) {
                 break;
             }
@@ -66,7 +66,7 @@
 
     // remove task from delay queue.
     fn.remove = function (taskId) {
-        this._notStartedNeeded('remove');
+        _notStartedNeeded.call(this, 'remove');
         // do nothing if taskId not find in index.
         if (!this._taskIdIndex.hasOwnProperty(taskId)) return;
         try {
@@ -78,7 +78,7 @@
     };
 
     // sorted tasks by duration
-    fn._getSortedTasksQueue = function () {
+    var _getSortedTasksQueue = function () {
         var resultQueue = [];
         for (var duration in this._delayMap) {
             if (this._delayMap.hasOwnProperty(duration)) {
@@ -93,17 +93,17 @@
         });
     };
 
-    fn._run = function () {
+    var _run = function () {
         var self = this;
         self._next = this.tasksQueue.shift();
         this._lastStartTime = Date.now();
         if (!self._next) return this._cb.fn && this._cb.fn.call(this._cb.context || this);
         self._timeout = setTimeout(function () {
-            return self._runNext();
+            return _runNext.call(self);
         }, self._next.duration - self._pre.duration);
     };
 
-    fn._runNext = function () {
+    var _runNext = function () {
         // run every tasks
         for (var taskId in this._next.tasks) {
             if (this._next.tasks.hasOwnProperty(taskId)) {
@@ -113,14 +113,14 @@
         }
         this._lastingTime = this._next.duration;
         this._pre = this._next;
-        return this._run();
+        return _run.call(this);
     };
 
     // run delay queue.
     fn.run = function (cb, context) {
-        this._notStartedNeeded('run');
+        _notStartedNeeded.call(this, 'run');
         this._started = true;
-        this.tasksQueue = this._getSortedTasksQueue();
+        this.tasksQueue = _getSortedTasksQueue.call(this);
         this._cb = {
             fn: cb,
             context: context
@@ -129,12 +129,12 @@
         this._pre = {
             duration: 0
         };
-        return this._run();
+        return _run.call(this);
     };
 
     // pause a delay queue after run.
     fn.pause = function () {
-        this._startedNeeded('parse');
+        _startedNeeded.call(this, 'parse');
         if (this._paused) return;
         this._paused = true;
         this._lastingTime -= this._lastStartTime - Date.now();
@@ -147,12 +147,12 @@
     // continue a delay if it has been paused.
     fn.cont = function () {
         var self = this;
-        this._startedNeeded('cont');
+        _startedNeeded.call(this, 'cont');
         if (!this._paused) return;
         this._paused = false;
         this._lastStartTime = Date.now();
         this._timeout = setTimeout(function () {
-            return self._runNext();
+            return _runNext.call(self);
         }, this._next.duration - this._lastingTime);
     };
 
